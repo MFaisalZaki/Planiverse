2713d35596590a3649cc717af1239722919294e6
Mustafa Faisal <mustafafaisal@mac.lan>
2025-04-11 20:12:03 +0200

    fixing pandemic issue.

--- planiverse/problems/real_world_problems/epidemic_control/environment.py
+++ planiverse/problems/real_world_problems/epidemic_control/environment.py
@@ -3,13 +3,38 @@
 import os
 import json
 import numpy as np
-from itertools import combinations, product
+from itertools import combinations, product, chain
 from gym import spaces
 from planiverse.problems.real_world_problems.epidemic_control.epipolicy.core.epidemic import construct_epidemic
 from planiverse.problems.real_world_problems.epidemic_control.epipolicy.obj.act import Act, construct_act, get_default_act, get_normalized_action
 from planiverse.problems.real_world_problems.base import RealWorldProblem
 
 PERIOD = 7
+
+class EpiAction:
+    def __init__(self, index, intervention_details):
+        self.name          = intervention_details.name
+        self.index         = index
+        self.locale_regex  = '*' 
+        self.cpv_list      = np.array([i.default_value for i in intervention_details.cp_list], dtype=np.float32)
+        self.default_value = self.cpv_list[0]
+        self.min_value     = self.cpv_list[1]
+        self.max_value     = self.cpv_list[2]
+        self.itv_details   = intervention_details
+    
+    def create_action(self, value):
+        ret_action = EpiAction(self.index, self.itv_details)
+        ret_action.cpv_list[0] = value
+        ret_action.default_value = value
+        return ret_action
+
+class EpiCost:
+    def __init__(self, index, intervention_details):
+        self.name          = intervention_details.name
+        self.index         = index
+        self.locale_regex  = '*' 
+        self.cpv_list      = np.array([i.default_value for i in intervention_details.cp_list], dtype=np.float32)
+        self.itv_details   = intervention_details
 
 class EpiState:
     def __init__(self, state, depth, static):
@@ -39,70 +64,24 @@ class EpiEnv(RealWorldProblem):
     def __reset__(self, session, vac_starts):
         self.epi = construct_epidemic(session)
         
-        action_count = 0
-        action_param_count =  0
-        for itv in self.epi.static.interventions:
-            if not itv.is_cost:
-                action_count += 1
-                action_param_count += len(itv.cp_list)
+        self.interventionslist = [] #list(map(lambda i: EpiAction(i[0], i[1]), enumerate(filter(lambda itv: not itv.is_cost, self.epi.static.interventions))))
+        self.costs             = [] #list(map(lambda i: EpiCost(i[0], i[1]), enumerate(filter(lambda itv: itv.is_cost, self.epi.static.interventions))))
+        self.interventions     = [] #list(chain.from_iterable([[itv.create_action(i) for i in np.linspace(itv.min_value, itv.max_value, 3)] for itv in self.interventionslist]))
         
-        self.act_domain = np.zeros((action_param_count, 2), dtype=np.float32)
-        
-        index = 0
-        for itv in self.epi.static.interventions:
-            if not itv.is_cost:
-                for cp in itv.cp_list:
-                    self.act_domain[index, 0] = cp.min_value
-                    self.act_domain[index, 1] = cp.max_value
-                    index += 1
-        # Define action and observation space
-        # They must be gym.spaces objects
-        # Example when using discrete actions:
-        # self.action_space = spaces.Box(low=0, high=1, shape=(action_count,), dtype=np.float32)
-        # I have my doubts about this actionlist, we need to recheck this.
+        for idx, itv in enumerate(self.epi.static.interventions):
+            if itv.is_cost:
+                self.costs += [EpiCost(idx, itv)]
+            else:
+                act = EpiAction(idx, itv)
+                self.interventions += [act.create_action(i) for i in np.linspace(act.min_value, act.max_value, 3)]
 
-        # interventionslist = list(filter(lambda itv: not itv.is_cost, self.epi.static.interventions))
-
-        # We need to create discretised actions parameters per intervention.
-        itv_ranges = []
-        for idx, itv in enumerate(self.epi.static.interventions): 
-            if itv.is_cost: continue
-            degree = list(filter(lambda d: d.name in ['degree', 'percentage', 'compliance'], itv.cp_list))[0]
-            itv_ranges.append(np.linspace(degree.min_value, degree.max_value, 3))
-
-        # Act(0, "*", [])
-        pass
-        self.actionslist  = list(product(*itv_ranges)) # list(map(lambda a: np.array(a),combinations([i*0.1 for i in range(0,10,4)], action_count)))
-        # Example for using image as input:
-        # self.observation_space = spaces.Box(low=0, high=total_population, shape=(obs_count,), dtype=np.float32)
         self.time_passed = 0 # To keep track of how many days have passed 
         self.vac_starts = vac_starts # number of days to prepare a vaccination / make it available 
-
-        # for dev only.
-        # self.epi.run(10)
     
     def __perform_action__(self, state, action):
-        # TODO: Do no action until the vaccination is available
-        # expanded_action = np.zeros(len(self.act_domain), dtype=np.float32)
-        # index = 0
-        # for i in range(len(self.act_domain)):
-        #     if self.act_domain[i, 0] == self.act_domain[i, 1]:
-        #         expanded_action[i] = self.act_domain[i, 0]
-        #     else:
-        #         expanded_action[i] = action[index]
-        #         index += 1
-        
-        # epi_action = []
-        # index = 0
-        # for itv_id, itv in enumerate(self.epi.static.interventions):
-        #     if not itv.is_cost:
-        #         epi_action.append(construct_act(itv_id, expanded_action[index:index+len(itv.cp_list)]))
-        #         index += len(itv.cp_list)
-        
-        for i in range(PERIOD):
-            normalized_action = get_normalized_action(self.epi.static, epi_action)
-            next_state, delta_parameter = self.epi.get_next_state(state.state, normalized_action)
-
+        next_state, delta_parameter = self.epi.get_next_state(state.state, action)
+        for i in range(PERIOD-1):
+            next_state, delta_parameter = self.epi.get_next_state(next_state, action)
         return EpiState(next_state, state.depth + PERIOD, self.epi.static)
 
     def reset(self):
@@ -129,9 +108,9 @@ class EpiEnv(RealWorldProblem):
     
     def successors(self, state):
         ret = []
-        for action in self.actionslist:
-            successor_state = self.__perform_action__(state, action)
+        for action in self.interventions:
+            successor_state = self.__perform_action__(state, [action] + self.costs)
             if successor_state == state: continue
             # we need to stringify the action for _BFS_SEARCH
-            ret.append(('^'.join([f'({idx}={v})' for idx, v in enumerate(action)]), successor_state))
+            ret.append((action, successor_state))
         return ret
