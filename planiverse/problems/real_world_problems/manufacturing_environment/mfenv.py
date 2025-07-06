1808e967125c0ce95dabc978171f8913171a83a8
Mustafa Faisal <mustafafaisal@Mustafas-MacBook-Pro-2.local>
2025-07-06 14:27:07 +0100

    improvements for the manfuct. env.

--- planiverse/problems/real_world_problems/manufacturing_environment/mfenv.py
+++ planiverse/problems/real_world_problems/manufacturing_environment/mfenv.py
@@ -9,16 +9,18 @@ from planiverse.problems.real_world_problems.base import RealWorldProblem
 # This environment is based on https://github.com/torayeff/mfgrl/tree/main
 
 class ActionType(Enum):
-    BUY_CFG = 0
-    CONTINUE_PRODUCTION = 1
-    FINISH_PRODUCTION_ALL = 2
-    FINISH_PRODUCTION_CFG = 3
+    BUY_CFG               = 0
+    CONTINUE_PRODUCTION   = 1
+    BATCH_PRODUCTION      = 2
+    FINISH_PRODUCTION_ALL = 3
+    FINISH_PRODUCTION_CFG = 4
 
 
 class ConfigurationAction:
-    def __init__(self, cfg_id, action_type:ActionType):
+    def __init__(self, cfg_id, action_type:ActionType, batch_size:int=-1):
         self.cfg = cfg_id
         self.action = action_type.value
+        self.batch_size = batch_size
 
     def __str__(self):
         match self.action:
@@ -30,6 +32,8 @@ class ConfigurationAction:
                 return 'finish_production_all'
             case ActionType.FINISH_PRODUCTION_CFG.value:
                 return f'finish_production_cfg_{self.cfg}'
+            case ActionType.BATCH_PRODUCTION.value:
+                return f'batch_production_cfg_{self.cfg}_size_{self.batch_size}'
             case _:
                 raise ValueError(f'Unknown action type: {self.action}')
     
@@ -50,7 +54,13 @@ class MfgState:
 
     def __update__(self):
         # frozenset([' ^ ' .join([f'{k}({str(v)})' for k, v in self._state.items()])])
-        self.literals = frozenset([f'{k}({str(v)})' for k, v in self._state.items()])
+        state_vars = []
+        for cfg, values in self._state["configuration_costs"].items():
+            state_vars.extend([f'{k}(cfg{cfg} {v})'.replace('.','').lower() for k, v in values.items()])
+        state_vars.append(f'demand({self._state["demand"]})'.replace('.','').lower())
+        state_vars.append(f'demand_time({self._state["demand_time"]})'.replace('.','').lower())
+
+        self.literals = frozenset(state_vars)
 
     def __eq__(self, state):
         return isinstance(state, MfgState) and self.literals == state.literals
@@ -69,25 +79,25 @@ class MfgState:
         ret_static_state = deepcopy(self._static_state)
 
         # calculate reward
-        reward = -1.0 * ret_state["market_incurring_costs"][cfg_id]
+        reward = -1.0 * ret_state['configuration_costs'][cfg_id]["market_incurring_costs"]
 
         # buy new configuration
         # update inucrred costs
-        ret_state["configuration_costs"][cfg_id]["incurred_costs"] = ret_state["market_incurring_costs"][cfg_id]
+        ret_state["configuration_costs"][cfg_id]["incurred_costs"] = ret_state['configuration_costs'][cfg_id]["market_incurring_costs"]
 
         # update recurring costs
-        ret_state["configuration_costs"][cfg_id]["recurring_costs"] = ret_state["market_recurring_costs"][cfg_id]
-        ret_static_state["recurring_costs"] = ret_state["market_recurring_costs"][cfg_id]
+        ret_state["configuration_costs"][cfg_id]["recurring_costs"] = ret_state['configuration_costs'][cfg_id]["market_recurring_costs"]
+        ret_static_state["recurring_costs"] = ret_state['configuration_costs'][cfg_id]["market_recurring_costs"]
 
         # update production rates
-        ret_state["configuration_costs"][cfg_id]["production_rates"] = ret_state["market_production_rates"][cfg_id]
-        ret_static_state["production_rates"] = ret_state["market_production_rates"][cfg_id]
+        ret_state["configuration_costs"][cfg_id]["production_rates"] = ret_state['configuration_costs'][cfg_id]["market_production_rates"]
+        ret_static_state["production_rates"] = ret_state['configuration_costs'][cfg_id]["market_production_rates"]
 
         # update setup times
-        ret_state["configuration_costs"][cfg_id]["setup_times"] = ret_state["market_setup_times"][cfg_id]
+        ret_state["configuration_costs"][cfg_id]["setup_times"] = ret_state['configuration_costs'][cfg_id]["market_setup_times"]
 
         # update cfgs status
-        ret_state["configuration_costs"][cfg_id]["cfgs_status"] = (1 / ret_state["market_setup_times"][cfg_id])
+        ret_state["configuration_costs"][cfg_id]["cfgs_status"] = (1 / ret_state['configuration_costs'][cfg_id]["market_setup_times"])
 
         # update production
         ret_state["configuration_costs"][cfg_id]["produced_counts"] = 0
@@ -125,6 +135,13 @@ class MfgState:
 
         return MfgState(ret_state, ret_static_state, self.DEMAND)
     
+    def batch_production(self, cfg_id, items):
+        next_state = self.copy()
+        for i in range(items):
+            next_state = next_state.continue_production(cfg_id)
+            if next_state.is_goal() or next_state.is_terminal(): break
+        return next_state
+
     def finish_production_all(self):
         ret_state = deepcopy(self._state)
         ret_static_state = deepcopy(self._static_state)
@@ -172,7 +189,9 @@ class MfgState:
                 # continue production for a specific configuration
                 while not (next_state.is_goal() or next_state.is_terminal()):
                     next_state = next_state.continue_production(action.cfg)
-                pass
+            case ActionType.BATCH_PRODUCTION.value:
+                # continue production for a specific configuration in batches
+                next_state = next_state.batch_production(action.cfg, action.batch_size)
             case ActionType.FINISH_PRODUCTION_ALL.value:
                 # continue production for all configurations
                 if not (next_state.is_goal() or next_state.is_terminal()) and\
@@ -206,6 +225,7 @@ class MfgEnv(RealWorldProblem):
             
             "configuration_costs" : {
                 i: {
+                    'id': i,
                     "bought": False,
                     "incurred_costs": 0,
                     "recurring_costs": 0,
@@ -213,13 +233,17 @@ class MfgEnv(RealWorldProblem):
                     "setup_times": 0,
                     "cfgs_status": 0,
                     "produced_counts": 0,
+                    "market_incurring_costs": self.INCUR_COSTS[i],
+                    "market_recurring_costs": self.RECUR_COSTS[i],
+                    "market_production_rates": self.PRODN_RATES[i],
+                    "market_setup_times": self.SETUP_TIMES[i],
                 } for i in range(self.NUM_CFGS)
             },
             
-            "market_incurring_costs": self.INCUR_COSTS,
-            "market_recurring_costs": self.RECUR_COSTS,
-            "market_production_rates": self.PRODN_RATES,
-            "market_setup_times": self.SETUP_TIMES,
+            # "market_incurring_costs": self.INCUR_COSTS,
+            # "market_recurring_costs": self.RECUR_COSTS,
+            # "market_production_rates": self.PRODN_RATES,
+            # "market_setup_times": self.SETUP_TIMES,
         }
 
         # static state are used for stochastic operations
@@ -242,17 +266,23 @@ class MfgEnv(RealWorldProblem):
         # so a successor state can be the following:
         # 1. purchasing a new configuration
         # 2. continuing production for existing configurations
-        # 3. or continuing production for all configurations
+        # 3. or continuing production for all configurations <- don't like this one
         for cfg_id, values in filter(lambda b: not b[1]['bought'], state._state["configuration_costs"].items()):
             buy_cfg_action =  ConfigurationAction(cfg_id, ActionType.BUY_CFG)
             successors.append((buy_cfg_action, state.apply_action(buy_cfg_action)))
         
         for cfg_id, values in filter(lambda b: b[1]['bought'], state._state["configuration_costs"].items()):
-            continue_production_action = ConfigurationAction(cfg_id, ActionType.CONTINUE_PRODUCTION)
-            successors.append((continue_production_action, state.apply_action(continue_production_action)))
-            finish_production_action = ConfigurationAction(cfg_id, ActionType.FINISH_PRODUCTION_CFG)
+            # continue_production_action = ConfigurationAction(cfg_id, ActionType.CONTINUE_PRODUCTION)
+            # successors.append((continue_production_action, state.apply_action(continue_production_action)))
+            for batch_size in [10, 20, 50, 100]:
+                batch_production_action = ConfigurationAction(cfg_id, ActionType.BATCH_PRODUCTION, batch_size)
+                # append the action to continue production for a specific configuration
+                successors.append((batch_production_action, state.batch_production(cfg_id, batch_size)))
+
+
+            # finish_production_action = ConfigurationAction(cfg_id, ActionType.FINISH_PRODUCTION_CFG)
             # append the action to continue production for a specific configuration
-            successors.append((finish_production_action, state.apply_action(finish_production_action)))
+            # successors.append((finish_production_action, state.apply_action(finish_production_action)))
         
         # append the action to continue production for all configurations if we have bought all configurations
         if len(list(filter(lambda b: not b[1]['bought'], state._state["configuration_costs"].items()))) == 0:
@@ -260,10 +290,16 @@ class MfgEnv(RealWorldProblem):
             # successors.append((FINISH_PRODUCTION_ALL_action, state.apply_action(FINISH_PRODUCTION_ALL_action)))
         return successors
         
-        
     def fix_index(self, index):
         assert index in self.data_index, f"Index {index} not found in data index."
         self._setup_data(self.data_index[index])
+    
+    def simulate(self, plan):
+        state, _ = self.reset()
+        ret_states_trace = [state]
+        for action in plan:
+            ret_states_trace.append(ret_states_trace[-1].apply_action(action))
+        return ret_states_trace
 
     def _check_problem_feasibility(self) -> bool:
         """Checks whether the problem is feasible.
