c3d7921d5c2c8e9219b1878a5aad1923835cfcb4
Mustafa Faisal <mustafafaisal@Mustafas-MacBook-Pro-3.local>
2025-08-10 15:44:16 +0100

    still work in progress urban planning application.

--- dev/dev.py
+++ dev/dev.py
@@ -1,9 +1,16 @@
 
-from planiverse.problems.real_world_problems.cyber_security_network_attack.network_attack import EnvNASim
 
-env = EnvNASim("tiny")
-
+from planiverse.problems.real_world_problems.urban_planning.environment import UrbanPlanningEnv
 
 
 
-pass
+env = UrbanPlanningEnv('Kendall Square', 
+                       "/Users/mustafafaisal/Developer/Planiverse/venv/lib/python3.12/site-packages/planiverse/problems/real_world_problems/urban_planning/cities/Kendall_Square_data",
+                       100)
+init_state, _ = env.reset()
+
+successors = env.successors(init_state)
+
+for action, state in successors:
+    print(f"Action: {action}, State: {state}")
+    pass
---
+++ planiverse/problems/real_world_problems/urban_planning/environment.py
@@ -0,0 +1,197 @@
+
+# This implementation is based on this paper:
+# "AI Agent as Urban Planner: Steering Stakeholder Dynamics in Urban Planning via Consensus-based Multi-Agent Reinforcement Learning"
+
+
+# So a state is a graph representing the urban environment.
+# The node represents a parcel which can be: residential zone 𝑟, office 𝑜, green space 𝑔, commercial zone 𝑐, facilities 𝑓
+# The edges represents the spatial relationships between these parcels.
+
+# As for the action, it is a reassignment of land use for a specific parcel.
+
+# Note files should be named as: node_info.csv and node_pairs_knn4.csv. We used the same format as the original dataset.
+
+# TODO: We need to figure out the numbers of the land type.
+
+import os
+import pandas as pd
+import networkx as nx
+from enum import Enum
+from copy import deepcopy
+
+class LandUseType(Enum):
+    RESIDENTIAL = 'r'
+    OFFICE      = 'o'
+    GREEN_SPACE = 'g'
+    COMMERCIAL  = 'c'
+    FACILITIES  = 'f'
+    EMPTY       = 'n'  # No land use, e.g., water bodies or undeveloped land
+
+class UrbanEnvState:
+    
+    def __init__(self, urban_graph, depth):
+        self.urban_graph = urban_graph
+        self.depth = depth
+    
+    def __eq__(self, other):
+        if not isinstance(other, UrbanEnvState): return False
+        # Two states are the same if they have the same land usage.
+        return nx.utils.misc.graphs_equal(self.urban_graph, other.urban_graph)
+
+
+# Use land actions.
+class UrbanPlanAction:
+    def __init__(self, name, action_str):
+        self.name = name
+        self.action_str = action_str
+    
+    def __str__(self):
+        return self.action_str
+    
+    def __get_lands_to_convert__(self, g, landtype:LandUseType):
+        return list(filter(lambda n: g.nodes[n]['type'] == landtype, g.nodes))
+
+    def apply(self, state):
+        # split half evenly empty space between r, o, g, c, f
+        landuse = deepcopy(state.urban_graph)
+        to_convert_lands = self.__get_lands_to_convert__(landuse, LandUseType.EMPTY)
+        if len(to_convert_lands) > 0: self.convert(landuse, to_convert_lands)
+        return UrbanEnvState(landuse, state.depth+1)
+    
+    def convert(self, landuse, to_convert_lands):
+        assert False, "This method should be implemented in the subclass."
+
+class ConvertEmptyAction(UrbanPlanAction):
+    def __init__(self):
+        super().__init__("Convert Empty", "TBD")
+
+    def convert(self, landuse, to_convert_lands):
+        # Convert half of the empty spaces evenly between r, o, g, c, f
+        to_convert_lands = to_convert_lands[:len(to_convert_lands)//2]
+        for land in to_convert_lands:
+            for new_type in [LandUseType.RESIDENTIAL, LandUseType.OFFICE, LandUseType.GREEN_SPACE, LandUseType.COMMERCIAL, LandUseType.FACILITIES]:
+                landuse.nodes[land]['type'] = new_type
+        
+
+class ConvertOfficesAction(UrbanPlanAction):
+    def __init__(self):
+        super().__init__("Convert Offices", "TBD")
+
+    def convert(self, landuse, to_convert_lands):
+        # So half of the offices will be splited 20% to be g and 80% to be commercial
+        to_convert_lands = to_convert_lands[:len(to_convert_lands)//2]
+        
+        to_green_space = to_convert_lands[:int(len(to_convert_lands)*0.2)]
+        to_commercial = to_convert_lands[int(len(to_convert_lands)*0.2):]
+        
+        for land in to_green_space:
+            landuse.nodes[land]['type'] = LandUseType.GREEN_SPACE
+        
+        for land in to_commercial:
+            landuse.nodes[land]['type'] = LandUseType.COMMERCIAL
+
+        
+class ConvertCommercialAction(UrbanPlanAction):
+    def __init__(self):
+        super().__init__("Convert Commercial", "TBD")
+
+    def convert(self, landuse, to_convert_lands):
+        # So half of the c will be spliited to 20% to be g and 80% to be f.
+        to_convert_lands = to_convert_lands[:len(to_convert_lands)//2]
+        to_green_space = to_convert_lands[:int(len(to_convert_lands)*0.2)]
+        to_facilities = to_convert_lands[int(len(to_convert_lands)*0.2):]
+
+        for land in to_green_space:
+            landuse.nodes[land]['type'] = LandUseType.GREEN_SPACE
+
+        for land in to_facilities:
+            landuse.nodes[land]['type'] = LandUseType.FACILITIES
+
+class ConvertFacilitesAction(UrbanPlanAction):
+    def __init__(self):
+        super().__init__("Convert Facilities", "TBD")
+
+    def convert(self, landuse, to_convert_lands):
+        # so 20% of the facilities will be converted to 20% green space and 80% to commercial.
+        to_convert_lands = to_convert_lands[:int(len(to_convert_lands)*0.2)]
+        to_green_space = to_convert_lands[:int(len(to_convert_lands)*0.2)]
+        to_commercial = to_convert_lands[int(len(to_convert_lands)*0.2):]
+
+        for land in to_green_space:
+            landuse.nodes[land]['type'] = LandUseType.GREEN_SPACE
+
+        for land in to_commercial:
+            landuse.nodes[land]['type'] = LandUseType.COMMERCIAL
+
+class UrbanPlanningEnv:
+    def __init__(self, urban_name:str, urban_info: str, horizon: int):
+        self.urban_name = urban_name
+        self.urban_info = urban_info
+        self.horizon    = horizon
+        
+        assert os.path.exists(urban_info), f"Urban info file {urban_info} does not exist."
+        assert os.path.exists(os.path.join(urban_info, 'node_info.csv')), f"node_info.csv file does not exist."
+        assert os.path.exists(os.path.join(urban_info, 'node_pairs_knn4.csv')), f"node_pairs_knn4.csv file does not exist."
+
+        self.node_info  = pd.read_csv(os.path.join(urban_info, 'node_info.csv'))
+        self.node_pairs = pd.read_csv(os.path.join(urban_info, 'node_pairs_knn4.csv'))
+
+        self.actions = [ConvertEmptyAction(), ConvertOfficesAction(), ConvertCommercialAction(), ConvertFacilitesAction()]
+
+
+    def reset(self):
+        # This is an initial map until I figure it out.
+        landuse_map = {
+            -1.0 : LandUseType.EMPTY,
+             0.0 : LandUseType.RESIDENTIAL,
+             1.0 : LandUseType.OFFICE,
+             2.0 : LandUseType.GREEN_SPACE,
+             3.0 : LandUseType.COMMERCIAL,
+             4.0 : LandUseType.FACILITIES
+        }
+
+        self.graph = nx.Graph()
+        for _, row in self.node_info.iterrows():
+            attributes = row.to_dict()
+            node_id = attributes.pop('node_id', None)
+            assert node_id is not None, "Node ID cannot be None."
+            attributes['type'] = landuse_map[attributes['landuse_type']]
+            self.graph.add_node(node_id, **attributes)
+        
+        for _, row in self.node_pairs.iterrows():
+            attributes = row.to_dict()
+            from_node = attributes.pop('node', None)
+            to_node = attributes.pop('node_adj', None)
+            assert from_node is not None and to_node is not None, "Node IDs cannot be None."
+            self.graph.add_edge(from_node, to_node, **attributes)
+
+        return UrbanEnvState(self.graph, 0), {}
+    
+    def fix_index(self, index):
+        pass
+
+    def is_goal(self, state):
+        return state.depth >= self.horizon
+    
+    def is_terminal(self, state):
+        # A better terminal state is the tree searched for 356 days.
+        return False
+    
+    # Returns a list of [action, successor_state]
+    def successors(self, state):
+        ret = []
+        for idx, action in enumerate(self.actions):
+            successor_state = action.apply(state)
+            if successor_state == state: continue
+            # we need to stringify the action for _BFS_SEARCH
+            ret.append((action, successor_state))
+        return ret
+    
+    def simulate(self, plan):
+        state, _ = self.reset()
+        ret_states_trace = [state]
+        for action in plan:
+            state = action.action.apply(state)
+            ret_states_trace.append(state)
+        return ret_states_trace
+    
